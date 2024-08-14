b3802c7ab5e312ee8bc78c89c66ce1e9085af3df
Anvarsha C <153602698+anvarshac@users.noreply.github.com>
2024-08-14 22:34:27 +0530

    Add files via upload

---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Loader.css'
+
+
+function Loader() {
+  return (
+    <div style={{display:'flex', justifyContent:'center'}}>
+        <div class="loader"></div>
+    </div>
+  )
+}
+
+export default Loader
